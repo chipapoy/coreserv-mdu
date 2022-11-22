8c74aeb2e27f506de097d9666578172727a7856c
chipapoy <mikdrake13@gmail.com>
2022-11-23 00:04:09 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    backendUrl: process.env.NEXT_PUBLIC_BACKEND_URL,
+  }
 }
 
 module.exports = nextConfig
