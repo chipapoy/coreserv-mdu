4f7fa38ccf5679cecc36ae01d95a6d3e36327bb5
unknown <MikSingson@SKYLT2498.skycable.com>
2022-11-24 03:14:55 +0800

    re update remove rewrite and headers

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const nextConfig = {
 //     // Will be available on both server and client
 //     backendUrl: process.env.NEXT_PUBLIC_BACKEND_URL,
 //   },
-<<<<<<< HEAD
   // async rewrites() {
   //   return [
   //     {
@@ -29,30 +28,6 @@ const nextConfig = {
   //     }
   //   ]
   // }
-=======
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://chipapoy.online/:path*'
-      }
-    ]
-  },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
->>>>>>> 8bd1ec989ad84850d04eabe8f2442d8c23245267
 }
 
 module.exports = nextConfig
