8359f9b075404441f9e537864bc67d7c64e9a785
unknown <MikSingson@SKYLT2498.skycable.com>
2022-11-25 03:37:01 +0800

    test csrf

--- hooks/auth.js
+++ hooks/auth.js
@@ -17,7 +17,7 @@ export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
             }),
     )
 
-    const csrf = () => axios.get('/sanctum/csrf-cookies')
+    const csrf = () => axios.get('/sanctum/csrf-cookie')
 
     const register = async ({ setErrors, ...props }) => {
         await csrf()
