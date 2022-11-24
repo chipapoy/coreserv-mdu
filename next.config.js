4664d2d33136905077e630960b0ae0a2dd0e2fc0
unknown <MikSingson@SKYLT2498.skycable.com>
2022-11-25 03:15:55 +0800

    add another path

--- next.config.js
+++ next.config.js
@@ -18,7 +18,10 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "/data/:path*",
+        source: [
+          "/data/:path*",
+          "/:path*"
+        ],
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
